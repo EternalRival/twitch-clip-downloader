e3247eca690415dd6be22a269c29390398516092
EternalRival <eternalrival8@gmail.com>
2022-08-19 07:16:14 +0300

    feat: add buttons functionality

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-const { app, BrowserWindow, dialog, ipcMain } = require("electron");
+const PROJECTNAME = "Twitch Clip Downloader";
+const electron = require("electron");
+const { app, BrowserWindow, clipboard, dialog, ipcMain, shell } = electron;
 const process = require("process");
 const path = require("path");
 const srcFolder = "src";
+const fs = require("fs");
 
+let win = null;
+let isFilePicked = false;
+let isDirPicked = false;
+let downloadDir = null;
+let jsonFile = null;
+
+app.disableHardwareAcceleration();
 app.whenReady().then(() => {
+  ipcMain.handle("getProjectName", () => PROJECTNAME);
+  ipcMain.handle("parseJson", parseJson);
+  ipcMain.handle("openURL", openURL);
+  ipcMain.handle("dialog:pickFile", handleFilePick);
   ipcMain.handle("dialog:pickDir", handleDirPick);
 
   createWindow();
@@ -17,22 +31,55 @@ app.on("window-all-closed", () => {
 });
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    title: PROJECTNAME,
     webPreferences: {
       preload: path.join(__dirname, srcFolder, "preload.js"),
     },
   });
-
-  win.loadFile(path.join(srcFolder, "index.html"));
+  win.loadFile(path.join(srcFolder, "index.html"))
+  //win.webContents.openDevTools();
 }
 
+async function handleFilePick() {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ["openFile"],
+    filters: [{ name: "*.json", extensions: ["json"] }],
+  });
+  if (!isFilePicked && canceled) return "⚠️ файл не выбран!";
+  isFilePicked = true;
+  jsonFile = filePaths[0] || jsonFile;
+  return [`📝 выбран файл ${jsonFile}`, path.basename(jsonFile)];
+}
 async function handleDirPick() {
-  console.log('duh')
-  const { cancelled, filePaths } = await dialog.showOpenDialog({
+  const { canceled, filePaths } = await dialog.showOpenDialog({
     properties: ["openDirectory"],
   });
-  if (!cancelled) return filePaths[0];
+  if (!isDirPicked && canceled) return "⚠️ папка не выбрана!";
+  isDirPicked = true;
+  downloadDir = filePaths[0] || downloadDir;
+
+  return [
+    `📁 выбрана папка ${downloadDir}`,
+    `${path.sep + path.dirname(downloadDir).split(path.sep).pop()}` +
+      `${path.sep + path.basename(downloadDir)}`,
+  ];
 }
 
+async function parseJson() {
+  clipboard.writeText(fs.readFileSync("./scripts/for-browser.js", "utf8"));
+  return "📑 код для devtools-консоли скопирован в буфер обмена";
+}
+
+async function openURL(_event, ...args) {
+  const nickname = args[0];
+  if (/^[a-zA-Z0-9][\w]{3,25}$/i.test(nickname)) {
+    const clipsURL = `https://dashboard.twitch.tv/u/${nickname}/content/clips/channel`;
+    setTimeout(() => shell.openExternal(clipsURL), 3000);
+
+    return `🌐 открываем ${clipsURL}`;
+  }
+  return "⚠️ введите корректный никнейм!";
+}
