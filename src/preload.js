d6055850d9a8ca0b1a269aec100a1812a69cdc27
EternalRival <eternalrival8@gmail.com>
2022-08-19 15:28:09 +0300

    refactor: revamp bridge

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("myAPI", {
-  projectName: (eventName, callback) => ipcRenderer.on(eventName, callback),
-  invoke: (eventName, ...args) => ipcRenderer.invoke(eventName, ...args),
-  send: (eventName, ...args) => ipcRenderer.send(eventName, ...args),
+  listen: (eventName, callback) => ipcRenderer.on(eventName, callback),
+  invoke: (eventName, args) => ipcRenderer.invoke(eventName, args),
+  send: (eventName, args) => ipcRenderer.send(eventName, args),
 });
 
